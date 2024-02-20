25469caa995f0b622bf228cb9a90f6d6ddf22711
Rishabh <rishabhsingh1597@gmail.com>
2024-02-20 15:43:55 -0500

    Added comments

--- Conditional-Statements_7/Program.cs
+++ Conditional-Statements_7/Program.cs
@@ -5,6 +5,10 @@ Comments: This C# Console application code demonstrates the use of
 conditional statements after getting input from users
 */
 
+//testing git push
+// test 1
+
+
 using System;
 // Ask the user for input
 Console.WriteLine("Please enter a Sales Amount ...");
